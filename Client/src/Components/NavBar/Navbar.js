50e355c41cff9ea7432c7d8c2fb37db30c5676ac
Moises Alvarez <mfalvare@espol.edu.ec>
2025-09-06 12:48:05 -0500

    cambiando el navbar para que sea rersponsive

--- Client/src/Components/NavBar/Navbar.js
+++ Client/src/Components/NavBar/Navbar.js
@@ -1,47 +1,40 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
-import logo from "./images/logo.png";
 import { ThemeContext } from "../../context/ThemeContext";
+import "./Navbar.css"; // Asegúrate de tener este archivo CSS
 
 const Navbar = (props) => {
   const { toggleTheme } = useContext(ThemeContext);
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!isMenuOpen);
 
   return (
-    <div className="navbar-container">
-      <div>
-        <Link className="logo-container" to="/">
-     
-          <p>{props.title}</p>
+    <nav className="navbar">
+      <div className="navbar-header">
+        <button className="menu-toggle" onClick={toggleMenu}>
+          ☰
+        </button>
+        <Link className="logo" to="/">
+          {props.title}
         </Link>
       </div>
-      <div>
-        <ul className="navbar-links">
-          <li>
-            <Link to="/">Inicio</Link>
-          </li>
-          <li>
-            <Link to="/servicios">Servicios</Link>
-          </li>
-          <li>
-            <Link to="/mascotas">Mascotas</Link>
-          </li>
-        
-        </ul>
-      </div>
-      <div className="nav-buttons-container">
-        <div className="theme-switcher">
+
+      <div className={`navbar-links ${isMenuOpen ? "open" : ""}`}>
+        <Link to="/" onClick={toggleMenu}>Inicio</Link>
+        <Link to="/servicios" onClick={toggleMenu}>Servicios</Link>
+        <Link to="/mascotas" onClick={toggleMenu}>Mascotas</Link>
+        <Link to="/servicios" className="cta" onClick={toggleMenu}>
+          Dar una Mascota 🐾
+        </Link>
+        <div className="theme-toggle">
           <label className="switch">
             <input type="checkbox" onClick={toggleTheme} />
             <span className="slider round"></span>
           </label>
         </div>
-        <div>
-          <Link to="/servicios">
-            <button className="Navbar-button">Dar una Mascota</button>
-          </Link>
-        </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
